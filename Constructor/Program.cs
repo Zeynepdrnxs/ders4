f1a78390ef6dbe56d600fbcd3385ac3a8d08bf18
Zeynepdrnxs <113857817+Zeynepdrnxs@users.noreply.github.com>
2023-10-15 00:05:21 +0300

    Add project files.

---
+++ Constructor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() { Id=1, FirstName="Engin", LastName="Demiroğ", City="Ankara"};
+            
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+
+            Customer customer2 = new Customer (2, "Zeynep", "DURNA", "İstanbul");
+            
+            Console.WriteLine(customer2.FirstName);
+
+        }
+    }
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        //default constructor parametresi olmayan constructor
+        public Customer(int id, string firstName, string lastName, string city) //metod parametreleri camelCase yazılır
+        {
+            //Console.WriteLine("Yapıcı blok çalıştı");
+            Id= id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int City { get; set; }
+
+    }
+}
---
+++ Generics/CustomDictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        MyDictionary<string, int> myDict = new MyDictionary<string, int>();
+
+        // MyDictionary sınıfını kullanarak anahtar-değer çiftleri ekleyelim
+        myDict.Add("Anahtar1", 42);
+        myDict.Add("Anahtar2", 123);
+
+        // Belirli anahtarları kullanarak değerlere erişelim ve ekrana yazdıralım
+        Console.WriteLine("Anahtar1: " + myDict.Get("Anahtar1"));
+        Console.WriteLine("Anahtar2: " + myDict.Get("Anahtar2"));
+
+        Console.ReadLine();
+    }
+}
+
+class MyDictionary<TKey, TValue>
+{
+    private Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+
+    // Yeni anahtar-değer çiftleri eklemek için kullanılır
+    public void Add(TKey key, TValue value)
+    {
+        dictionary[key] = value;
+    }
+
+    // Belirli bir anahtarın değerini döndürür
+    public TValue Get(TKey key)
+    {
+        // Eğer anahtar Dictionary içinde varsa, değeri döndürür
+        if (dictionary.ContainsKey(key))
+        {
+            return dictionary[key];
+        }
+        // Eğer anahtar bulunamazsa, bir hata fırlatır
+        throw new KeyNotFoundException("Anahtar bulunamadı");
+    }
+}
